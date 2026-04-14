ee4626f90f2f23a92b94f32e2553025f37c684f2
Khaled Allam <kaaoaa505@gmail.com>
2026-04-14 16:44:40 +0300

    route with agent prompt

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Ai\Agents\LocalAssistant;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/ai', function () {
+    $response = LocalAssistant::make()->prompt("write my name from arabic '
+    خالد علام احمد عمر
+    ' to every language in the world,
+     separated by <br> before and after");
+
+    return (string) $response;
 });
